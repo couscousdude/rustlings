af10e404ebdbb86dfe68754038b6bc054cd4dca2
Youwen Wu <youwenw@gmail.com>
2024-03-15 13:59:47 -0700

    finished 35/96 (2 hrs)

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(q: u16) -> u32 {
+    if q > 40 {
+        q.into()
+    } else {
+        (q * 2).into()
+    }
+}
 
 // Don't modify this function!
 #[test]
